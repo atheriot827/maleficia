c213b184c63037cda4c04b52eb7d69a723a7734c
atheriot827 <atheriot827@gmail.com>
2025-08-19 09:53:01 -0500

    Initial commit

---
+++ webpack.config.js
@@ -0,0 +1,106 @@
+const webpack = require('webpack');
+const path = require('path');
+const CopyPlugin = require('copy-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const ReactRefreshWebpackPlugin = require('@pmmmwh/react-refresh-webpack-plugin');
+
+module.exports = (env, argv) => {
+  const isDevelopment = argv.mode === 'development';
+
+  return {
+    entry: './src/index.tsx',
+    output: {
+      path: path.resolve(__dirname, 'dist'),
+      filename: isDevelopment ? '[name].[hash].js' : '[name].[contenthash].js'
+    },
+    module: {
+      rules: [
+        {
+          test: /\.(js|jsx|ts|tsx)$/,
+          use: {
+            loader: 'babel-loader',
+            options: {
+              plugins: [
+                isDevelopment && require.resolve('react-refresh/babel')
+              ].filter(Boolean)
+            }
+          },
+          exclude: /node_modules/
+        },
+        {
+          test: /\.css$/,
+          use: [
+            'style-loader',
+            {
+              loader: 'css-loader',
+              options: {
+                importLoaders: 1
+              }
+            },
+            'postcss-loader'
+          ],
+          exclude: /\.module\.css$/
+        },
+        {
+          test: /\.css$/,
+          use: [
+            'style-loader',
+            {
+              loader: 'css-loader',
+              options: {
+                importLoaders: 1,
+                modules: true
+              }
+            },
+            'postcss-loader'
+          ],
+          include: /\.module\.css$/
+        },
+        {
+          test: /\.png$/,
+          use: [
+            {
+              loader: 'url-loader',
+              options: {
+                mimetype: 'image/png'
+              }
+            }
+          ]
+        }
+      ]
+    },
+    devServer: {
+      static: {
+        directory: './dist'
+      },
+      hot: true
+    },
+    plugins: [
+      new CopyPlugin({
+        patterns: [{ from: 'src/index.html' }],
+      }),
+      new HtmlWebpackPlugin({
+        templateContent: ({ htmlWebpackPlugin }) => '<!DOCTYPE html><html><head><meta charset="utf-8"><title>' + htmlWebpackPlugin.options.title + '</title></head><body><div id="app"></div></body></html>',
+        filename: 'index.html',
+      }),
+      new CleanWebpackPlugin(),
+      isDevelopment && new ReactRefreshWebpackPlugin()
+    ].filter(Boolean),
+    resolve: {
+      extensions: ['.tsx', '.ts', '.js']
+    },
+    optimization: {
+      runtimeChunk: 'single',
+      splitChunks: {
+        cacheGroups: {
+          vendor: {
+            test: /[\\/]node_modules[\\/]/,
+            name: 'vendors',
+            chunks: 'all'
+          }
+        }
+      }
+    }
+  };
+};
