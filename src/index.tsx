a90b71f4e5ea8f12de28070ef46fbc2449c1761c
atheriot827 <atheriot827@gmail.com>
2025-08-23 06:05:30 -0500

    change: make app compatiable with React 18/19

--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,11 @@
 import * as React from 'react';
-import * as ReactDOM from "react-dom";
+import * as ReactDOM from "react-dom/client"; // <-- use 'react-dom/client' for React 18+
 
 import App from './App';
 import "./styles.css";
 
-var mountNode = document.getElementById("app");
-ReactDOM.render(<App name="Jane" />, mountNode);
+const mountNode = document.getElementById("app");
+if (mountNode) {
+  const root = ReactDOM.createRoot(mountNode);
+  root.render(<App name="Jane" />);
+}
